79f725543fc8424ac25d5086d8a287037184e757
Ivan Shibkikh <ishibkikh@wiley.com>
2018-03-30 17:33:36 +0300

    -02 optimization

---
+++ cmake-build-release-mingw/CMakeFiles/oauth_lite.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/ishibkikh/CLionProjects/liboauth/library.c" "C:/Users/ishibkikh/CLionProjects/liboauth/cmake-build-release-mingw/CMakeFiles/oauth_lite.dir/library.c.obj"
+  "C:/Users/ishibkikh/CLionProjects/liboauth/src/base64.c" "C:/Users/ishibkikh/CLionProjects/liboauth/cmake-build-release-mingw/CMakeFiles/oauth_lite.dir/src/base64.c.obj"
+  "C:/Users/ishibkikh/CLionProjects/liboauth/src/memxor.c" "C:/Users/ishibkikh/CLionProjects/liboauth/cmake-build-release-mingw/CMakeFiles/oauth_lite.dir/src/memxor.c.obj"
+  "C:/Users/ishibkikh/CLionProjects/liboauth/src/oauth.c" "C:/Users/ishibkikh/CLionProjects/liboauth/cmake-build-release-mingw/CMakeFiles/oauth_lite.dir/src/oauth.c.obj"
+  "C:/Users/ishibkikh/CLionProjects/liboauth/src/sha1.c" "C:/Users/ishibkikh/CLionProjects/liboauth/cmake-build-release-mingw/CMakeFiles/oauth_lite.dir/src/sha1.c.obj"
+  "C:/Users/ishibkikh/CLionProjects/liboauth/src/xmalloc.c" "C:/Users/ishibkikh/CLionProjects/liboauth/cmake-build-release-mingw/CMakeFiles/oauth_lite.dir/src/xmalloc.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release-mingw/CMakeFiles/test_liboath.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_liboath.dir/main.c.obj"
+  "CMakeFiles/test_liboath.dir/src/oauth.c.obj"
+  "CMakeFiles/test_liboath.dir/src/sha1.c.obj"
+  "CMakeFiles/test_liboath.dir/src/memxor.c.obj"
+  "CMakeFiles/test_liboath.dir/src/base64.c.obj"
+  "CMakeFiles/test_liboath.dir/src/xmalloc.c.obj"
+  "test_liboath.pdb"
+  "test_liboath.exe"
+  "test_liboath.exe.manifest"
+  "libtest_liboath.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_liboath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
