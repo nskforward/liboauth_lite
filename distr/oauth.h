6b180b0f69e7cfecfc5c0c8f50a64b610e94e5c4
Ivan Shibkikh <ishibkikh@wiley.com>
2018-03-30 12:14:31 +0300

    release

--- distr/oauth.h
+++ distr/oauth.h
@@ -16,4 +16,5 @@ void oauth_sign_post(char* http_method,
     lr_save_string(lr_eval_string(http_post_body), "OAUTH_PARAMETERS");
     oauth_sign_body(consumer_key, consumer_secret, http_method, lr_eval_string("{OAUTH_URL}"), lr_eval_string("{OAUTH_PARAMETERS}"), out_body);
     lr_save_string(out_body, "OAUTH_SIGNED_BODY");
+    free(out_body);
 }
